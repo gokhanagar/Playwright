227bcd5f33182d6f51770429414574051cb90f00
gokhanagar <gokhan.agar@momentup.com>
2023-12-25 09:32:37 +0300

    V1

---
+++ src/test/java/Ders1_TarayiciAcma/TarayiciAcma.java
@@ -0,0 +1,42 @@
+package Ders1_TarayiciAcma;
+
+import com.microsoft.playwright.*;
+
+import java.awt.*;
+
+
+public class TarayiciAcma {
+
+
+    public static void main(String[] args) {
+
+            Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+            int width = (int) dimension.getWidth();
+            int height = (int) dimension.getHeight();
+
+            Playwright playwright = Playwright.create();
+            Browser browser = playwright.chromium().launch(
+                    new BrowserType.LaunchOptions().setHeadless(false));
+
+
+            BrowserContext context = browser.newContext(new Browser.NewContextOptions().setViewportSize(width,height));
+
+
+            Page page = context.newPage();
+
+            //page.setViewportSize(width,height);
+
+            page.navigate("http://playwright.dev");
+
+            System.out.println(page.title());
+
+
+            page.close();
+            browser.close();
+
+            playwright.close();
+
+
+        }
+
+    }
