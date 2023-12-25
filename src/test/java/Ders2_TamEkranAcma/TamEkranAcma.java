91ae10e9aea803669fe439c9c242bf16e0565312
gokhanagar <gokhan.agar@momentup.com>
2023-12-26 02:50:44 +0300

    V2

---
+++ src/test/java/Ders2_TamEkranAcma/TamEkranAcma.java
@@ -0,0 +1,40 @@
+package Ders2_TamEkranAcma;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+import java.awt.*;
+
+public class TamEkranAcma {
+    public static void main(String[] args) throws InterruptedException {
+
+    Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+    int width = (int) dimension.getWidth();
+    int height = (int) dimension.getHeight();
+
+
+    Playwright playwright = Playwright.create();
+    Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+    Page page = browser.newPage();
+
+    page.navigate("https://www.hepsiburada.com/");
+
+    page.setViewportSize(width,height);
+
+
+    Thread.sleep(3000);
+
+    page.close();
+    browser.close();
+
+    playwright.close();
+
+
+
+
+
+
+    }
+}
